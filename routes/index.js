97858391e4df99b0d0e32ef3ad9eacb0e3639408
androidengel <androidengel@gmail.com>
2019-11-05 14:56:47 -0600

    Adding Ajax REST API for dropdown search functionality.

--- routes/index.js
+++ routes/index.js
@@ -52,5 +52,11 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)
 );
 
+/*
+  API
+*/
+
+router.get('/api/search', catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 module.exports = router;
